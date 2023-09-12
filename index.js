584f6ea563d4eb141338a5a62c57363eed8d8993
Jesse Reynolds <jessereynolds18@gmail.com>
2023-09-11 22:51:59 -0500

    create code to output a string when the DOM has loaded completely

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    let firstText = document.querySelector("p");
+    firstText.textContent = "This is really cool!";
+    console.log(firstText.textContent);
+})
+
+console.log(document.querySelector("p").textContent);
